fc2d72bad8dc880158b595d0db8d28b69105457d
weixiumei <997817485@qq.com>
2017-02-26 23:19:21 +0800

    结构未完。。

--- JS 图片画廊/image.js
+++ JS 图片画廊/image.js
@@ -30,21 +30,43 @@ $(document).ready(function(){
     //循环显示文件夹里的图片
     //$.getJSON(url,[data],[callback])
     var i = 1;
-    var imagesDiv = document.getElementsByTagName("div")[0];
+    var imagesSec = document.getElementsByClassName("img-sec");
     $.getJSON("./data/imageDatas.json",function(data){
         data.forEach(function(value){
-            var image1 = document.createElement("img");
-            image1.setAttribute("src","./images/"+value.fileName);
-            image1.setAttribute("id","image"+(i++));
-            imagesDiv.appendChild(image1);
+            var figure = document.createElement("figure");
+            figure.setAttribute("class","img-figure");
+            figure.setAttribute("id","img-figure"+(i++));
+
+            var img = document.createElement("img");
+            img.setAttribute("src","./images/"+value.fileName);
+            img.setAttribute("alt",value.title);
+
+            figure.insertAfter(img);
+            imagesSec.appendChild(figure);
         })
     })
     // 布局图片初始化
-    rearrange(1);
+    init();
+
     // 设置图片角度
 })
+
 // 翻转行为
 
+function init(){
+    //加载以后，为每张图片计算其位置的范围。
+    //首先拿到舞台的大小
+
+    //拿到一个图片的大小
+    //计算中心图片的位置点
+    //左侧区域排布位置x的取值范围
+    //右侧区域排布位置x的取值范围
+    //左右侧区域排布位置y的取值范围
+    //上测区域排布位置的取值范围
+
+    //重新布局所有图片
+    rearrange(0);
+}
 /**
  * 重新布局所有图片
  * @param 指定居中排布哪个图片
